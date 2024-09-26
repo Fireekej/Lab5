3f90c0ed847bac8ef4dc10933350935624faed1d
Fireekej <103969082+Fireekej@users.noreply.github.com>
2024-09-26 20:25:17 +0300

    Add files via upload

---
+++ Лаб5_2.py
@@ -0,0 +1,21 @@
+seasons = int(input("Enter the number from 1 to 4:   "))
+
+if seasons == 1:
+    print("Winter")
+
+if seasons == 2:
+    print("Spring")
+
+if seasons == 3:
+    print("Summer")
+
+if seasons == 4:
+    print("Autumn")
+
+
+if seasons > 4:
+     print("Error")
+
+
+if seasons < 1:
+     print("Error")
